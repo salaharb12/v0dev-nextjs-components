e9c9cf8b6fc0dac907d9bc220105af8cd0c47be7
salah <salah@192.168.1.13>
2024-09-01 15:23:55 +0100

    add breadcrumb cammand and other ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Analytics } from "@/components/analytics";
 import { CalendarForm } from "@/components/CalendarForm";
+import { InputOTPForm } from "@/components/InputOtp";
+import { Drawer } from "@/components/ui/drawer";
 
 export default function Home() {
   return (
     <main className="p-40">
       <CalendarForm />
+      <InputOTPForm />
     </main>
   );
 }
